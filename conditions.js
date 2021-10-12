1db7442e13bae8dcf447a3fad6417f2c43bb5565
Melissa Medeiros Teodoro <honeythisa@gmail.com>
2021-10-12 01:38:17 -0300

    commit of the folder
    commit

---
+++ conditions.js
@@ -0,0 +1,19 @@
+let velo = 32;
+
+console.log(`A velocidade de seu carro é ${velo}km/h`);
+// condição simples
+if (velo > 60) {
+    console.log("Você ultrapassou a velocidade permitida. MULTADO!");
+}
+console.log("Dirija sempre usando cinto de segurança!");
+
+let country = "EUA";
+
+console.log(`Vivendo em ${country}`);
+// condição composta
+if (country != "Brasil") {
+    console.log("Você é estrangeiro");
+
+} else {
+    console.log("Você é brasileiro");
+}
